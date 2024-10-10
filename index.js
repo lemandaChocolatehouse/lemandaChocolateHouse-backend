4f5e500825c36e80c782d0e6c508933975394c86
Aftab1311 <md.aftabehsan@gmail.com>
2024-10-10 15:48:48 +0530

    index.js updated

--- index.js
+++ index.js
@@ -8,7 +8,28 @@ const bcrypt = require("bcrypt");
 require("dotenv").config();
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173", // for development
+  "https://www.lemanda.in", // for production
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+  })
+);
+
+app.options("*", cors());
+
 
 
 const productsRouter = require("./routers/products");
